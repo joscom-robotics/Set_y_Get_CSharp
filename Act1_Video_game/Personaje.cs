4461a98d19db8c2b93f25671e199f8f5fd7aa0bf
Jos <jos1230@gmail.com>
2022-10-08 20:26:49 -0500

    Add project files.

---
+++ Act1_Video_game/Personaje.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Act1_Video_game
+{
+    public class Personaje
+    {
+        String anexo;
+        private String mensaje2 = "Esta es otra persona. ";
+        public void mensaje(){Console.WriteLine("!Esta es una persona.");}
+        ///----------
+        public string Mensaje2
+        {
+            set{mensaje2 = value;}
+            get{return mensaje2;}
+        }
+        public string Anexo{
+            set{anexo = value;}
+            get{return anexo;}
+        }
+        public void ambos()
+        {
+            Console.WriteLine(Mensaje2 + " " + Anexo);
+        }
+    }
+}
+ 
---
+++ Act1_Video_game/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Act1_Video_game;
+
+Personaje mono = new Personaje();
+
+mono.mensaje();
+mono.Anexo = "Y este es el amigo.";
+
+Console.WriteLine(mono.Mensaje2 + "Ok.");
+Console.WriteLine(mono.Anexo);
+
+mono.ambos();
+
+Console.ReadKey();
